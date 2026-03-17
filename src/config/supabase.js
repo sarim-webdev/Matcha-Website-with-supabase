9f7d1e0f54db99e289618ae5d859498658780440
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-03-17 18:38:45 +0500

    react

--- src/config/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://gnjaazooijiktkmewtpp.supabase.co";
-const supabaseAnonKey = "sb_publishable_NGKvDsuTJ9sXFbH7CoJqmQ_aY13UITo";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
