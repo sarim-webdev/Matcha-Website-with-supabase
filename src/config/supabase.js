25a4339e013923a84ff274ef2cdbbf2850be2853
Muhammad Sarim <muhammadsarim1861@gmail.com>
2026-03-17 18:34:00 +0500

    react

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://gnjaazooijiktkmewtpp.supabase.co";
+const supabaseAnonKey = "sb_publishable_NGKvDsuTJ9sXFbH7CoJqmQ_aY13UITo";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
